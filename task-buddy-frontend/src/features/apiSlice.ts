f7b605f12f989bde4f559ca6a4bcd343e88a4537
danielmalurica <danielmalurica@gmail.com>
2024-04-03 21:00:49 +0300

    Base URL for RTK Query changed

--- task-buddy-frontend/src/features/apiSlice.ts
+++ task-buddy-frontend/src/features/apiSlice.ts
@@ -2,7 +2,7 @@ import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react'
 
 export const apiSlice = createApi({
     reducerPath: 'taskApi',
-    baseQuery: fetchBaseQuery({ baseUrl: 'http://localhost:3001/',  prepareHeaders: (headers) => {
+    baseQuery: fetchBaseQuery({ baseUrl: 'https://taskbuddybackend.onrender.com/',  prepareHeaders: (headers) => {
         headers.set("Content-Type", "application/json");
         return headers;
       }, credentials: "include"}),
